e074defdedb79c2e7833427bf7080eac78eedc91
Jallen31-Paper <64849924+Jallen31-Paper@users.noreply.github.com>
2021-08-17 16:23:33 +0800

    todoformjs-u1

---
+++ src/TodoForm.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const TodoForm = ({ todo }) => {
+
+    const {register, handleSubmit} = useForm({ 
+        defaultValues: { text: todo ? todo.text : "" },
+    });
+    const history = useHistory()
+
+    const onSubmit = handleSubmit((data) => {
+        alert(JSON.stringify(data));
+        history.push("/")
+    });
+    return (
+        <div className="container">
+            <div className="mt-3">
+                <h3>Edit Todo Item</h3>
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="text">
+                           Text: 
+                        </label> 
+                        <input className="form-control" {...register('text')} type="text" name="text" id="text"/>
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-primary">
+                            Create Todo
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
